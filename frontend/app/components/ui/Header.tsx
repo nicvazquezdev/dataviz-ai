20fe5b102b28ac4dea1b07c0471924f3fbdf80bb
Nicolas Vazquez <nicorvazquezs@gmail.com>
2025-07-24 19:41:09 -0300

    css: sticky header

--- frontend/app/components/ui/Header.tsx
+++ frontend/app/components/ui/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header className="bg-white shadow-md">
+    <header className="bg-white shadow-md sticky top-0">
       <div className="container mx-auto px-4 py-6">
         <Link href={"/"} className="flex items-center gap-3">
           <div className="flex h-10 w-10 items-center justify-center rounded-lg bg-gradient-to-br from-blue-500 to-purple-600">
